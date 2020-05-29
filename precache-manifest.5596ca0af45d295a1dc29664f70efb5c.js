de32be8e1e7b49fe4d47ba79ed0a2d6b7ce5ff5e
Saada2016 <saadakhan2016@outlook.com>
2020-05-29 12:45:16 -0800

    Updates

---
+++ precache-manifest.5596ca0af45d295a1dc29664f70efb5c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f6ac2ab8a9d3259deeed0165f1a2b8",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "71ea3ebfc93e0494473f",
+    "url": "/myPortfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "86a85442b5cf8e455bd4",
+    "url": "/myPortfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "71ea3ebfc93e0494473f",
+    "url": "/myPortfolio/static/js/2.d5063707.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myPortfolio/static/js/2.d5063707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a85442b5cf8e455bd4",
+    "url": "/myPortfolio/static/js/main.4910bf1a.chunk.js"
+  },
+  {
+    "revision": "68915027288fd00f3cc4",
+    "url": "/myPortfolio/static/js/runtime-main.8afba0d3.js"
+  },
+  {
+    "revision": "7331dd63de1cb3be431a547be0049db2",
+    "url": "/myPortfolio/static/media/image1.7331dd63.jpeg"
+  },
+  {
+    "revision": "8d57039dc28b952526f85b58a6ac4369",
+    "url": "/myPortfolio/static/media/image2.8d57039d.jpeg"
+  },
+  {
+    "revision": "22a6477bb63598410640710dbfa11872",
+    "url": "/myPortfolio/static/media/image3.22a6477b.jpeg"
+  },
+  {
+    "revision": "345511f9b6955202fbf71d078e81c11a",
+    "url": "/myPortfolio/static/media/pic1.345511f9.jpg"
+  }
+]);
